91db31aee636bebc34638693ad48ddfb90f96735
Santiago Andres Del Valle Pinilla <santiagoandresdelvallepinilla@gmail.com>
2022-10-04 23:36:14 -0500

    v2

--- src/typescript/main.js
+++ src/typescript/main.js
@@ -1,9 +1,17 @@
 "use strict";
+var _a, _b, _c;
 class game {
     constructor(minimo, maximo) {
         this.data = [];
+        this.tamano = 0;
         this.minimo = minimo;
         this.maximo = maximo;
+    }
+    set_tamano(tamano) {
+        this.tamano = tamano;
+    }
+    get_tamano() {
+        return this.tamano;
     }
     get_minimo() {
         return this.minimo;
@@ -22,15 +30,23 @@ class game {
             return false;
         }
     }
+    reset() {
+        this.data = [];
+        this.tamano = 0;
+    }
 }
-var data = new game(1, 100);
+var data;
 function generate_number() {
+    data = new game(1, 100);
     for (var i = 0; i < 25; i++) {
-        var numero_mon = Math.floor(Math.random() * (data.get_maximo() - data.get_minimo() + 1) + data.get_minimo());
+        var numero_mon = Math.floor(Math.random() * (data.get_maximo() - data.get_minimo() + 1) +
+            data.get_minimo());
         while (data.get_data().includes(numero_mon) == true) {
-            numero_mon = Math.floor(Math.random() * (data.get_maximo() - data.get_minimo() + 1) + data.get_minimo());
+            numero_mon = Math.floor(Math.random() * (data.get_maximo() - data.get_minimo() + 1) +
+                data.get_minimo());
         }
         data.get_data().push(numero_mon);
+        data.set_tamano(data.get_data().length);
     }
 }
 function game_start() {
@@ -48,12 +64,58 @@ function evaluar(x) {
     var plantilla = document.getElementById(x);
     if (plantilla.style.backgroundColor == "black") {
         plantilla.style.backgroundColor = "white";
-        data.get_data().push(parseInt(x));
+        plantilla.style.color = "black";
+        data.set_tamano(data.get_tamano() + 1);
     }
     else {
         plantilla.style.backgroundColor = "black";
-        data.get_data().splice(parseInt(x));
+        plantilla.style.color = "white";
+        data.set_tamano(data.get_tamano() - 1);
     }
-    console.log(data.get_data());
+    console.log(data.get_tamano());
 }
+(_a = document.getElementById("ver_a")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", (e) => {
+    var plantilla = document.getElementById("id01");
+    if (data.get_tamano() == 0) {
+        plantilla.style.display = "block";
+        var plantilla_med = document.getElementById("x_a");
+        plantilla_med.innerHTML = "Ganaste";
+    }
+    else {
+        plantilla.style.display = "block";
+        var plantilla_med = document.getElementById("x_a");
+        plantilla_med.innerHTML = "Perdiste";
+    }
+});
+(_b = document.getElementById("ver_a_b")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", (e) => {
+    var plantilla_main = document.getElementById("otro_numero");
+    if (plantilla_main.style.backgroundColor != "orange") {
+        plantilla_main.style.backgroundColor = "orange";
+    }
+    var plantilla = document.getElementById("carta_principal");
+    var random = Math.floor(Math.random() * (data.get_maximo() - data.get_minimo() + 1) +
+        data.get_minimo());
+    var sub_plantilla = plantilla.innerHTML;
+    plantilla.innerHTML = `<p id="numero_carta_principal" class="numero_carta_principal">${random}</p>`;
+    if (sub_plantilla != "") {
+        var xd = document.getElementById("carta_secundaria_row");
+        xd.innerHTML += `<div class="col-4">
+        <p class="contenedor_carta_secundaria">
+            ${sub_plantilla}
+        </p>
+    </div>`;
+    }
+});
+(_c = document.getElementById("ver_a_b_c")) === null || _c === void 0 ? void 0 : _c.addEventListener(("click"), (e) => {
+    data.reset();
+    var plantilla = document.getElementById("contenedor");
+    plantilla.innerHTML = "";
+    var plantilla_main = document.getElementById("otro_numero");
+    plantilla_main.style.background = "white";
+    var plantilla_primaria_carta = document.getElementById("carta_principal");
+    plantilla_primaria_carta.innerHTML = "";
+    var plantilla_secundaria_carta = document.getElementById("carta_secundaria_row");
+    plantilla_secundaria_carta.innerHTML = "";
+    game_start();
+});
 game_start();
--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,70 +1,143 @@
 class game {
-    data: Array<number> = []
-    minimo: number
-    maximo: number
-    constructor(minimo: number, maximo: number) {
-        this.minimo = minimo
-        this.maximo = maximo
-    }
-    public get_minimo():number {
-        return this.minimo
-    }
+  data: Array<number> = [];
+  minimo: number;
+  maximo: number;
+  tamano: number = 0;
 
-    public get_maximo():number {
-        return this.maximo
-    }
+  constructor(minimo: number, maximo: number) {
+    this.minimo = minimo;
+    this.maximo = maximo;
+  }
+  public set_tamano(tamano: number): void {
+    this.tamano = tamano;
+  }
 
-    public get_data():Array<number> {
-        return this.data
-    }
+  public get_tamano(): number {
+    return this.tamano;
+  }
 
-    public isdata():boolean{
-        if(this.data.length == 0 ){
-            return true
-        }else{
-            return false
-        }
+  public get_minimo(): number {
+    return this.minimo;
+  }
+
+  public get_maximo(): number {
+    return this.maximo;
+  }
+
+  public get_data(): Array<number> {
+    return this.data;
+  }
+
+  public isdata(): boolean {
+    if (this.data.length == 0) {
+      return true;
+    } else {
+      return false;
     }
+  }
+
+  public reset():void{
+    this.data = []
+    this.tamano = 0
+  }
 }
 
-var data = new game(1, 100)
+var data: game;
 
 function generate_number() {
-    for (var i = 0; i < 25; i++) {
-        var numero_mon = Math.floor(Math.random() * (data.get_maximo() - data.get_minimo() + 1) +data.get_minimo())
-        while (data.get_data().includes(numero_mon) == true) {
-            numero_mon = Math.floor(Math.random() * (data.get_maximo() - data.get_minimo() + 1) +data.get_minimo())
-        }
-        data.get_data().push(numero_mon)
+  data = new game(1, 100);
+  for (var i = 0; i < 25; i++) {
+    var numero_mon = Math.floor(
+      Math.random() * (data.get_maximo() - data.get_minimo() + 1) +
+        data.get_minimo()
+    );
+    while (data.get_data().includes(numero_mon) == true) {
+      numero_mon = Math.floor(
+        Math.random() * (data.get_maximo() - data.get_minimo() + 1) +
+          data.get_minimo()
+      );
     }
+    data.get_data().push(numero_mon);
+    data.set_tamano(data.get_data().length);
+  }
 }
 
 function game_start() {
-    var plantilla = document.getElementById("contenedor") as HTMLDivElement
-    generate_number()
-    for (var i = 0; i < data.get_data().length; i++) {
-        plantilla.innerHTML += `<div id="${data.get_data()[i]}"  class="col-2 numero" onclick="evaluar(${data.get_data()[i]})">
+  var plantilla = document.getElementById("contenedor") as HTMLDivElement;
+  generate_number();
+  for (var i = 0; i < data.get_data().length; i++) {
+    plantilla.innerHTML += `<div id="${
+      data.get_data()[i]
+    }"  class="col-2 numero" onclick="evaluar(${data.get_data()[i]})">
         <p class="numero_contenido">
             ${data.get_data()[i]}
         </p>
-    </div>`
-    }
+    </div>`;
+  }
 }
 
-function evaluar(x: string):void {
-    var plantilla = document.getElementById(x) as HTMLDivElement
-    if (plantilla.style.backgroundColor == "black") {
-        plantilla.style.backgroundColor = "white"
-        data.get_data().push(parseInt(x))
-    } else {
-        plantilla.style.backgroundColor = "black"
-        data.get_data().splice(parseInt(x))
-    }
-    console.log(data.get_data())
-
+function evaluar(x: string): void {
+  var plantilla = document.getElementById(x) as HTMLDivElement;
+  if (plantilla.style.backgroundColor == "black") {
+    plantilla.style.backgroundColor = "white";
+    plantilla.style.color = "black";
+    data.set_tamano(data.get_tamano() + 1);
+  } else {
+    plantilla.style.backgroundColor = "black";
+    plantilla.style.color = "white";
+    data.set_tamano(data.get_tamano() - 1);
+  }
+  console.log(data.get_tamano());
 }
 
-game_start()
+document.getElementById("ver_a")?.addEventListener("click", (e) => {
+  var plantilla = document.getElementById("id01") as HTMLDivElement;
+  if (data.get_tamano() == 0) {
+    plantilla.style.display = "block";
+    var plantilla_med = document.getElementById("x_a") as HTMLParagraphElement;
+    plantilla_med.innerHTML = "Ganaste";
+  } else {
+    plantilla.style.display = "block";
+    var plantilla_med = document.getElementById("x_a") as HTMLParagraphElement;
+    plantilla_med.innerHTML = "Perdiste";
+  }
+});
 
+document.getElementById("ver_a_b")?.addEventListener("click", (e) => {
+  var plantilla_main = document.getElementById("otro_numero") as HTMLDivElement;
+  if (plantilla_main.style.backgroundColor != "orange") {
+    plantilla_main.style.backgroundColor = "orange";
+  }
+  var plantilla = document.getElementById(
+    "carta_principal"
+  ) as HTMLParagraphElement;
+  var random = Math.floor(
+    Math.random() * (data.get_maximo() - data.get_minimo() + 1) +
+      data.get_minimo()
+  );
+  var sub_plantilla = plantilla.innerHTML;
+  plantilla.innerHTML = `<p id="numero_carta_principal" class="numero_carta_principal">${random}</p>`;
+  if (sub_plantilla != "") {
+    var xd = document.getElementById("carta_secundaria_row") as HTMLDivElement;
+    xd.innerHTML += `<div class="col-4">
+        <p class="contenedor_carta_secundaria">
+            ${sub_plantilla}
+        </p>
+    </div>`;
+  }
+});
 
+document.getElementById("ver_a_b_c")?.addEventListener(("click"),(e)=>{
+    data.reset()
+    var plantilla = document.getElementById("contenedor") as HTMLDivElement;
+    plantilla.innerHTML = ""
+    var plantilla_main = document.getElementById("otro_numero") as HTMLDivElement;
+    plantilla_main.style.background = "white"
+    var plantilla_primaria_carta = document.getElementById("carta_principal") as HTMLDivElement
+    plantilla_primaria_carta.innerHTML =""
+    var plantilla_secundaria_carta = document.getElementById("carta_secundaria_row") as HTMLDivElement
+    plantilla_secundaria_carta.innerHTML = ""
+    game_start()
+})
 
+game_start();
